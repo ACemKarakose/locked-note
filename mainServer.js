6c09695d8f8cb7b5df2988e0594a2ccf6872e2b0
Cem Karakose <cem.karakose@mertech.com.tr>
2022-11-04 15:06:52 +0300

    second test

--- mainServer.js
+++ mainServer.js
@@ -5,8 +5,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("HELLO FROM BACKEND");
+  res.send("HELLO FROM BACKEND CEM TEST");
 });
 const server = app.listen(process.env.PORT || 3000, function () {
   console.log("SERVER WORKING");
 });
+
+// https://lock-note-server.herokuapp.com/
